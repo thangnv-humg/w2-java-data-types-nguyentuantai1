838f94534c37af8870a645d149946491e7becea2
unknown <btvekinhpcuata@gmail.com>
2025-01-16 14:53:44 +0700

    nop bai tap nguyen tuan tai

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
+
 public class Main {
     public static void main(String[] args) {
         // TODO: Khai báo biến và viết chương trình theo yêu cầu trong README.md
-
         // Ví dụ:
         // int myInt = 42;
         // double myDouble = 3.14;
@@ -9,5 +9,20 @@ public class Main {
         // System.out.println(myInt);
         // System.out.println(myDouble);
         // System.out.println(myString);
+        int intValue = 42;
+        double doubleValue = 3.14;
+        String stringValue = "Hello, Java!";
+
+        // In giá trị các biến ra màn hình
+        System.out.println("intValue: " + intValue);
+        System.out.println("doubleValue: " + doubleValue);
+        System.out.println("stringValue: " + stringValue);
+
+        // Kiểm tra giá trị của biến intValue
+        if (intValue > 10) {
+            System.out.println("Greater than 10");
+        } else {
+            System.out.println("Less or equal to 10");
+        }
     }
 }
